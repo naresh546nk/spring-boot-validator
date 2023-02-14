14830d177034edcba796f073f8a7b7520febe20f
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2023-02-14 12:32:51 +0530

    test

--- src/main/java/com/developer/SpringBootValidatorApplication.java
+++ src/main/java/com/developer/SpringBootValidatorApplication.java
@@ -1,12 +1,12 @@
 package com.developer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootValidatorApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootValidatorApplication.class, args);
 	}
